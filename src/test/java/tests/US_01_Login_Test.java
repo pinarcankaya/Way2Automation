0b14b912c6c7edfc3da6c2f32fbc2e6ac20fcd6c
pinarcankaya <pinarunalcan50@gmail.com>
2020-10-12 12:09:07 +0200

    local pinardan duzeltme

--- src/test/java/tests/US_01_Login_Test.java
+++ src/test/java/tests/US_01_Login_Test.java
@@ -1,4 +1,10 @@
 package tests;
 
-public class US_01_Login_Test {
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class US_01_Login_Test extends TestBase {
+    @Test
+    public void TC_001() {
+    }
 }
