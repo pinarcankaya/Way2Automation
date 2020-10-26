6a3384794bf45523a9471deaebafab471c2c04c3
azramelek <gsoybelli@gmail.com>
2020-10-26 11:49:09 -0500

    local gulserden us04

---
+++ src/test/java/pages/US_04_Page.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class US_04_Page {
+    public US_04_Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    @FindBy(xpath = "//a[.='Dynamic Elements']")
+    public WebElement dynamicElements;
+
+    @FindBy(xpath = "//a[.='Submit Button Clicked']")
+    public WebElement submitButtonMenu;
+
+    @FindBy(xpath = "//ul[@class='responsive-tabs']/li")
+    public List<WebElement> submitButtonList;
+    //@FindBy(xpath = "//a[@target='_self']")
+    //public List<WebElement> submitButtonList2;
+    @FindBy(xpath = "//input[@type='text']")
+    public WebElement startsWithTestBox;
+    @FindBy(xpath= "//input[@name='submit']")
+    public WebElement startsWithsubmitButton;
+  @FindBy(xpath = "//a[.='Ends With']")
+    public WebElement endsWith;
+}
+
+
