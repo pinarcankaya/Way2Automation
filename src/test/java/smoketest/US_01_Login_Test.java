adb5eefacf04da27b5d034f0abc724d854b3c3d8
zuhreatay <atayzuhre61@gmail.com>
2021-07-06 17:14:01 -0500

    from zuhre branch

--- src/test/java/smoketest/US_01_Login_Test.java
+++ src/test/java/smoketest/US_01_Login_Test.java
@@ -1,128 +1,45 @@
 package smoketest;
 
 
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.US_01_Login_Page;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 import utilities.TestBase;
 
-
-
+import java.util.concurrent.TimeUnit;
 
 public class US_01_Login_Test extends TestBase {
 
-    US_01_Login_Page us01LoginPage=new US_01_Login_Page();
+    US_01_Login_Page us01LoginPage = new US_01_Login_Page();
 
-
+    @BeforeClass
+    public void setup() {
+        Driver.getDriver().get(ConfigReader.getProperty("way2Automation_url"));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        ReusableMethods.waitFor(5);
+    }
 
     @Test
     public void TC_001() {
         //1)musteri http://way2automation.com/way2auto_jquery/index.php web sayfasina girebiliyor olmali
+        System.out.println(us01LoginPage.RegistrationFormText.getText());
         Assert.assertTrue(us01LoginPage.RegistrationFormText.isDisplayed());
     }
 
     @Test
-    public void TC_002() {
-
-       // 1)dogru username ve dogru password ile giris yapilabiliyor olmal
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("valid_username"));
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
-
-        us01LoginPage.submitButton.click();
-    wait.until(ExpectedConditions.visibilityOf(us01LoginPage.homeText));
-        Assert.assertTrue(us01LoginPage.homeText.isDisplayed());
-
-        //1)dogru username ve dogru password ile giris yapilabiliyor olmali
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("valid_username"));
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
-        us01LoginPage.submitButton.click();
-        //wait eklenebilir
-        Assert.assertTrue(us01LoginPage.HomeText.isDisplayed());
-
+    public void TC_02() {
+        //2) ENTER TO THE TESTING WEBSIDE ile giris yapilabiliyor olmali
+        us01LoginPage.enterGiris.click();
+        System.out.println(us01LoginPage.scriptsPlace.getText());
+        Assert.assertTrue(us01LoginPage.scriptsPlace.isDisplayed());
     }
-
-    @Test
-    public void TC_003() {
-        //1)yanlis username, dogru password ile giris yapilamamali
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("invalid_username"));
-
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
-        us01LoginPage.submitButton.click();
-
-        wait.until(ExpectedConditions.visibilityOf(us01LoginPage.loginErrorMesaj));
-        Assert.assertTrue(us01LoginPage.loginErrorMesaj.isDisplayed());
-
-
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("invalid_password"));
-        us01LoginPage.submitButton.click();
-        wait.until(ExpectedConditions.visibilityOf(us01LoginPage.loginErrorMesaj));
-        Assert.assertTrue(us01LoginPage.loginErrorMesaj.isDisplayed());
-
-    }
-
-    @Test
-    public void TC_004() {
-
-        //dogru username,yanlis password  giris yapilmamali
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("valid_username"));
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("invalid_password"));
-
-        //1)yanlis username, dogru password ile giris yapilamamali
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("invalid_username"));
-        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("valid_password"));
-
-        us01LoginPage.submitButton.click();
-
-        wait.until(ExpectedConditions.visibilityOf(us01LoginPage.loginErrorMesaj));
-        Assert.assertTrue(us01LoginPage.loginErrorMesaj.isDisplayed());
-    }
-
-    @Test
-    public void TC_005() {
-        //yanlis username,yanlis password giris yapilmamali
-        us01LoginPage.signinButton.click();
-        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("invalid_username"));
-
-//
-//    @Test
-//    public void TC_005() {
-//       // 2)dogru username, yanlis password ile giris yapilamamali
-//        us01LoginPage.signinButton.click();
-//        us01LoginPage.usernameTextBox.sendKeys(ConfigReader.getProperty("valid_username"));
-//
-//        us01LoginPage.passwordTextBox.sendKeys(ConfigReader.getProperty("invalid_password"));
-//        us01LoginPage.submitButton.click();
-//
-//        wait.until(ExpectedConditions.visibilityOf(us01LoginPage.loginErrorMesaj));
-//        Assert.assertTrue(us01LoginPage.loginErrorMesaj.isDisplayed());
-//    }
-
-    //@AfterClass
-    //public void tearDown(){
-      //  Driver.closeDriver();
-    //}
-}
-
-
     @AfterClass
-    public void tearDown() {
+    public void cloes(){
         Driver.closeDriver();
     }
-
-
-
-
 }
 
